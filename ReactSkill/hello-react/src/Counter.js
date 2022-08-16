be383137202b0e12238e1646fe2f8fb04b0bf260
Yeyeong Yu <79914479+uyeah0@users.noreply.github.com>
2022-08-16 16:49:33 +0900

    [리액트를 다루는 기술]  3.4.1.1 state 객체 안에 여러 값이 있을 때 - #4

--- ReactSkill/hello-react/src/Counter.js
+++ ReactSkill/hello-react/src/Counter.js
@@ -5,14 +5,16 @@ class Counter extends Component{
         super(props);
         // state 초기값 설정
         this.state = {
-            number: 0
+            number: 0,
+            fixedNumber: 0
         };
     }
     render(){
-        const { number } = this.state; // state를 조회할 때는 this.state로 조회한다
+        const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회한다
         return (
             <div>
                 <h1>{number}</h1>
+                <h2>바뀌지 않는 값: {fixedNumber}</h2>
                 <button
                 // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정한다
                 onClick={()=>{
