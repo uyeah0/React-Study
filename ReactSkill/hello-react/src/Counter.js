1ae0dc9a5bfcad2e81c221afa882b248e98377c1
Yeyeong Yu <79914479+uyeah0@users.noreply.github.com>
2022-08-16 16:40:26 +0900

    Add files via upload

---
+++ ReactSkill/hello-react/src/Counter.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+class Counter extends Component{
+    constructor(props){ // 컴포넌트에 state를 설정할 때 
+        super(props);
+        // state 초기값 설정
+        this.state = {
+            number: 0
+        };
+    }
+    render(){
+        const { number } = this.state; // state를 조회할 때는 this.state로 조회한다
+        return (
+            <div>
+                <h1>{number}</h1>
+                <button
+                // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정한다
+                onClick={()=>{
+                    // this.setState를 사용하여 state에 새로운 값을 넣을 수 있다
+                    this.setState({number: number+1});
+                }}
+                >
+                    +1
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
