628eb42d51d62190675713b76d86ff787d3270bc
uyeah0 <uyeah029@gmail.com>
2022-09-26 14:16:24 +0900

    [리액트를 다루는 기술] 8장 Hooks #7 - 8.2.2 특정 값이 업데이트될 때만 실행
    useEffect의 두 번재 파라미터로 전달되는 배열 안에 검사하고 싶은 값 넣기

--- ReactSkill/hello-react/src/Info.js
+++ ReactSkill/hello-react/src/Info.js
@@ -5,8 +5,8 @@ const Info = () => {
   const [nickname, setNickname] = useState('');
 
   useEffect(() => {
-    console.log('마운트될 때만 실행됩니다.');
-  }, []);
+    console.log(name);
+  }, [name]);
 
   const onChangeName = (e) => {
     setName(e.target.value);
