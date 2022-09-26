9c5b433f996c1ec75d7389e5abbdb070e4d3e19e
uyeah0 <uyeah029@gmail.com>
2022-09-26 13:52:51 +0900

    [리액트를 다루는 기술] 8장 Hooks #7 - 8.2 useEffect
    useEffect 는 리액트 컴포넌트가 렌더링 될 때마다 특정 작업을 수행하도록 설정할 수 있는 Hooks
    -> 클래스형 컴포넌트의 componentDidMount와 componentDidUpdate를 합친 형태

--- ReactSkill/hello-react/src/Info.js
+++ ReactSkill/hello-react/src/Info.js
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Info = () => {
   const [name, setName] = useState('');
   const [nickname, setNickname] = useState('');
+
+  useEffect(() => {
+    console.log('렌더링이 완료되었습니다!');
+    console.log({
+      name,
+      nickname,
+    });
+  });
 
   const onChangeName = (e) => {
     setName(e.target.value);
