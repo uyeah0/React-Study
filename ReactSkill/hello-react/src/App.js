8cecef7064b7e78a4244397a9d5f0e73fb93db65
uyeah0 <uyeah029@gmail.com>
2022-09-26 13:39:33 +0900

    [리액트를 다루는 기술] 8장 Hooks - #7 8.1.1 useState 여러 번 사용하기

--- ReactSkill/hello-react/src/App.js
+++ ReactSkill/hello-react/src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
-import Counter from "./Counter";
+import React from 'react';
+import Info from './Info';
 
 const App = () => {
-  return <Counter />;
+  return <Info />;
 };
 
 export default App;
